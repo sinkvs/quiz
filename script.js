2ac660c7f6f5f29d0626681bdc6426d4928a3a32
sinkvs <sinkevichvs@vk.com>
2025-03-05 18:32:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function loadQuestion() {
         if (currentQuestionIndex >= questions.length) {
-            quizContent.style.display = 'none';
             finalButton.style.display = 'block';
             return;
         }
