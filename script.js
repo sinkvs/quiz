1acaf7661130acc0c4605c72be13b29b6c6680e5
sinkvs <sinkevichvs@vk.com>
2025-03-06 15:17:04 +0600

    12312314

--- script.js
+++ script.js
@@ -1,102 +1,87 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const startLeo = document.getElementById('start-leo');
-    const buke = document.getElementById('buke');
-    const quizContainer = document.getElementById('quiz-container');
-    const questionElement = document.getElementById('question');
-    const answersElement = document.getElementById('answers');
-    const errorAudio = document.getElementById('errorAudio');
-    const goodAudio = document.getElementById('goodAudio');
+document.addEventListener('DOMContentLoaded', function() {
+    const startImage = document.querySelector('.start-screen img');
+    const startScreen = document.getElementById('startScreen');
+    const quizContent = document.getElementById('quizContent');
     const backgroundAudio = document.getElementById('backgroundAudio');
+    const finalButton = document.createElement('button');
+    finalButton.id = 'finalButton';
+    finalButton.textContent = 'И в завершение';
+    document.body.appendChild(finalButton);
 
-    const questions = [
-        // Ваши вопросы здесь
-    ];
-
-    let currentQuestionIndex = 0;
-    let isInteractionAllowed = true; // Флаг для защиты от быстрого нажатия
-
-    startLeo.addEventListener('dblclick', () => {
-        startLeo.style.display = 'none';
-        buke.style.display = 'block';
-        quizContainer.style.display = 'block';
+    startImage.addEventListener('dblclick', function() {
+        startScreen.style.display = 'none';
+        quizContent.style.display = 'flex';
         backgroundAudio.play().catch(error => {
             console.error('Ошибка воспроизведения аудио:', error);
         });
         loadQuestion();
     });
 
+    const questions = [
+        {
+            question: "Какой инструмент используется для создания объема у корней волос?",
+            options: ["A) Плойка", "B) Утюжок", "C) Брашинг", "D) Фен с насадкой-диффузором"],
+            correctAnswer: "C"
+        },
+        {
+            question: "Какой тип стрижки лучше всего подходит для тонких волос?",
+            options: ["A) Каскад", "B) Пикси", "C) Боб", "D) Лесенка"],
+            correctAnswer: "A"
+        },
+        {
+            question: "Какой продукт используется для фиксации прически и придания ей блеска?",
+            options: ["A) Мусс", "B) Лак для волос", "C) Воск", "D) Гель"],
+            correctAnswer: "B"
+        }
+    ];
+
+    let currentQuestionIndex = 0;
+    const questionText = document.getElementById('questionText');
+    const options = document.querySelectorAll('.option');
+    const errorAudio = document.getElementById('errorAudio');
+    const goodAudio = document.getElementById('goodAudio');
+    const yesImage = document.getElementById('yesImage');
+    const noImage = document.getElementById('noImage');
+
     function loadQuestion() {
         if (currentQuestionIndex >= questions.length) {
-            buke.style.display = 'none';
-            quizContainer.style.display = 'none';
+            quizContent.style.display = 'none';
+            finalButton.style.display = 'flex';
             return;
         }
-
         const currentQuestion = questions[currentQuestionIndex];
-        questionElement.innerText = currentQuestion.question;
-        answersElement.innerHTML = '';
-        currentQuestion.answers.forEach((answer, i) => {
-            const answerElement = document.createElement('div');
-            answerElement.classList.add('answer');
-            answerElement.innerText = answer;
-            answerElement.dataset.answer = answer[0]; // Устанавливаем значение для dataset
-            answerElement.addEventListener('click', () => checkAnswer(answer[0]));
-            answersElement.appendChild(answerElement);
+        questionText.textContent = currentQuestion.question;
+        options.forEach((option, index) => {
+            option.textContent = currentQuestion.options[index];
+            option.dataset.answer = currentQuestion.options[index].charAt(0);
+            option.classList.remove('wrong', 'correct');
         });
-        isInteractionAllowed = true; // Сбрасываем флаг при загрузке нового вопроса
     }
 
-    function checkAnswer(selectedAnswer) {
-        if (!isInteractionAllowed) return; // Проверяем флаг
+    options.forEach(option => {
+        option.addEventListener('click', function() {
+            const selectedAnswer = this.dataset.answer;
+            const correctAnswer = questions[currentQuestionIndex].correctAnswer;
 
-        isInteractionAllowed = false; // Устанавливаем флаг, чтобы предотвратить быстрое нажатие
+            if (selectedAnswer === correctAnswer) {
+                goodAudio.play();
+                this.classList.add('correct');
+                yesImage.classList.add('slide-in-out');
+                setTimeout(() => {
+                    yesImage.classList.remove('slide-in-out');
+                    currentQuestionIndex++;
+                    loadQuestion();
+                }, 4000);
+            } else {
+                errorAudio.play();
+                this.classList.add('wrong');
+                noImage.classList.add('slide-in-out');
+                setTimeout(() => {
+                    noImage.classList.remove('slide-in-out');
+                }, 4000);
+            }
+        });
+    });
 
-        const correctAnswer = questions[currentQuestionIndex].correctAnswer;
-
-        if (selectedAnswer === correctAnswer) {
-            goodAudio.play();
-            document.querySelectorAll('.answer').forEach(el => el.classList.remove('correct', 'wrong'));
-            document.querySelector(`[data-answer="${selectedAnswer}"]`).classList.add('correct');
-            showCorrectImage();
-        } else {
-            errorAudio.play();
-            document.querySelectorAll('.answer').forEach(el => el.classList.remove('correct', 'wrong'));
-            document.querySelector(`[data-answer="${selectedAnswer}"]`).classList.add('wrong');
-            showIncorrectImage();
-        }
-    }
-
-    function showCorrectImage() {
-        let yesImage;
-        if (currentQuestionIndex < 5) {
-            yesImage = document.getElementById('alexYesImage');
-        } else if (currentQuestionIndex < 10) {
-            yesImage = document.getElementById('vladYesImage');
-        } else if (currentQuestionIndex < 15) {
-            yesImage = document.getElementById('kostyaYesImage');
-        } else {
-            yesImage = document.getElementById('vovaYesImage');
-        }
-
-        yesImage.style.opacity = 1;
-        yesImage.classList.add('slide-in-out');
-        setTimeout(() => {
-            yesImage.style.opacity = 0;
-            yesImage.classList.remove('slide-in-out');
-            currentQuestionIndex++;
-            loadQuestion();
-        }, 4000);
-    }
-
-    function showIncorrectImage() {
-        const noImage = currentQuestionIndex < 10 ? document.getElementById('alexEndImage') : document.getElementById('vovaEndImage');
-
-        noImage.style.opacity = 1;
-        noImage.classList.add('slide-in-out');
-        setTimeout(() => {
-            noImage.style.opacity = 0;
-            noImage.classList.remove('slide-in-out');
-            isInteractionAllowed = true; // Разрешаем взаимодействие после завершения анимации
-        }, 4000);
-    }
+    loadQuestion();
 });
